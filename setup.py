ee8b4d76a22f25bab3e42de2f291fd625054a9e6
Sebastian Ramacher <sebastian+dev@ramacher.at>
2013-04-11 20:07:24 +0200

    Add classifiers and long_description
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 
+import os.path
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+
+def read(name):
+  f = open(os.path.join(os.path.dirname(__file__), name))
+  ret = f.read()
+  f.close()
+  return ret
 
 setup(
   name="libdiscid",
   version="0.1",
-  description="Python bindings of libdiscid",
+  description="Python bindings for libdiscid",
+  long_description=read("README.rst"),
   author="Sebastian Ramacher",
   author_email="sebastian+dev@ramacher.at",
   url="https://github.com/sebastinas/python-libdiscid",
@@ -28,5 +36,15 @@ setup(
     "setuptools"
   ],
   test_suite="libdiscid.tests",
-  use_2to3=True
+  use_2to3=True,
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Topic :: Multimedia :: Sound/Audio :: CD Audio :: CD Ripping",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+  ]
 )
