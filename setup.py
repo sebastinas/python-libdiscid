15ace1956f7e7dff37a4b75981aa718b9f82235d
Sebastian Ramacher <sebastian+dev@ramacher.at>
2013-04-11 13:00:34 +0200

    Rename to libdiscid
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 setup(
-  name="discid",
+  name="libdiscid",
   version="0.1",
   description="Python binding of libdiscid",
   author="Sebastian Ramacher",
   author_email="sebastian+dev@ramacher.at",
   license="Expat",
   ext_modules = cythonize([
-    Extension("discid", ["discid.pyx", "discid-wrapper.c"])
+    Extension("libdiscid", ["discid.pyx", "discid-wrapper.c"])
   ]),
   install_requires = [
     "cython",
